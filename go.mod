ae87f0b4bdc66eff0d111e0cb202203b46f90d3c
Harokku <crenna.simone@gmail.com>
2021-03-08 15:35:02 +0100

    Refactored code to use local env file (yml format) or path variables
    Restructured server.go

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber/v2 v2.5.0
-	github.com/joho/godotenv v1.3.0
+	github.com/lib/pq v1.10.0
 )
--- server.go
+++ server.go
@@ -1,45 +1,102 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	_ "github.com/lib/pq"
+	"internal-backend/utils"
 	"log"
-	"os"
+	"time"
 )
 
 func main() {
-	// Variable def
+	// -------------------------
+	// Variable definition
+	// -------------------------
+
 	var (
-		err error
+		err   error
+		port  string //server port from env
+		dbUrl string //database url
+		conn  *sql.DB
 	)
 
-	// Load .env file
-	log.Println("Loading .env file")
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	log.Println("Successfully loaded .env file")
+	log.Printf("Starting environment init...")
+	initStartTime := time.Now() //Startup timer start
 
-	// Fiber definition
+	// -------------------------
+	// .env file loading
+	// -------------------------
+
+	// Read server port from env
+	port, err = utils.ReadEnv("PORT")
+	if err != nil {
+		log.Fatalf("Fatal error setting server port: %v", err)
+	}
+	log.Printf("Server port set to: %v", port)
+
+	// Read db url
+	dbUrl, err = utils.ReadEnv("DATABASE_URL")
+	if err != nil {
+		log.Fatalf("Fatal error setting database url: %v", err)
+	}
+	log.Printf("DB url set")
+
+	// -------------------------
+	// DB pool connection
+	// -------------------------
+	conn, err = sql.Open("postgres", dbUrl)
+	if err != nil {
+		log.Fatalf("Can't connect to db: %v", err)
+	}
+	log.Printf("Connection string set")
+
+	defer conn.Close()
+
+	// Try ping db to check for availability
+	err = conn.Ping()
+	if err != nil {
+		log.Fatalf("Can't ping database %v", err)
+	}
+	log.Printf("DB correctly pinged")
+
+	// -------------------------
+	// Fiber definition and server start
+	// -------------------------
+
 	app := fiberApp()
 
-	app.Listen(fmt.Sprintf(":%s", os.Getenv("PORT")))
+	initDuration := time.Since(initStartTime) //calculate total startup time
+	log.Printf("Enviromnent initialized in %s", initDuration)
+
+	err = app.Listen(fmt.Sprintf(":%s", port))
+	if err != nil {
+		log.Fatalf("Error starting server: %s", err)
+	}
 
 }
 
+// Create a new fiber app and define routes
 func fiberApp() *fiber.App {
 	var (
 		// err error
 		app *fiber.App
 	)
 	app = fiber.New()
+	app.Use(logger.New())
 
-	// Static route
+	// -------------------------
+	// Static routes
+	// -------------------------
+
 	app.Static("/", "./static")
 
+	// -------------------------
 	// Debug routes
+	// -------------------------
+
 	app.Get("/ping", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("pong")
 	})
