36aeb82e28cc128cbb698a3e6a3d6b0966cc5828
Harokku <crenna.simone@gmail.com>
2021-03-22 15:42:07 +0100

    Implemented crawler.go with helper.go functions to enumerate files in doc root extracting required informations

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 var (
@@ -17,14 +18,16 @@ var (
 // Walk through filesystem and enumerate found files in db, hashing them
 func EnumerateDocuments() error {
 	var (
-		err      error
-		docsRoot string //Documents disk path
+		err     error
+		docRoot string //Documents disk path
 	)
+	log.Printf("Starting enumerating documents...")
+	enumerateStartTime := time.Now()
 
 	// -------------------------
 	// Read .env
 	// -------------------------
-	docsRoot, err = utils.ReadEnv("DOC_ROOT")
+	docRoot, err = utils.ReadEnv("DOC_ROOT")
 	if err != nil {
 		log.Fatalf("Error retrieving documents root from env")
 	}
@@ -35,16 +38,53 @@ func EnumerateDocuments() error {
 	}
 
 	//TODO: Implement filewalker to enum existent files
-	if err = filepath.Walk(docsRoot, addDir); err != nil {
+	if err = filepath.Walk(docRoot, addDir); err != nil {
 		return err
 	}
+
+	enumerateDuration := time.Since(enumerateStartTime) //calculate total startup time
+	log.Printf("Document enumerated in %s", enumerateDuration)
 
 	return nil
 }
 
 //TODO: Implement function to add file to db after hashing it
 func addDir(path string, fi os.FileInfo, err error) error {
-	fmt.Printf("path: %v\n", path)
-	fmt.Printf("fi: %v\n", fi)
+	var (
+		category     string //Calculated category based on path
+		displayName  string //Calculated display name based on filename
+		sha1Checksum string //SHA-1 filename hash
+	)
+	//TODO: Remove printf
+	fmt.Printf("Path: %v\n", path)
+
+	// Check if file is not dir and process it
+	if !fi.IsDir() {
+
+		//Call helper func to extract category from path as relative path from document root
+		category, err = getCategory(path)
+		if err != nil {
+			log.Printf("error retrieving category: %v\n", err)
+		}
+		//TODO: Remove printf
+		fmt.Printf("category: %v\n", category)
+
+		//Call helper func to extract display name from path
+		displayName = getDisplayNameFromPath(path)
+		if err != nil {
+			log.Printf("error retrieving display name: %v\n", err)
+		}
+		//TODO: Remove printf
+		fmt.Printf("displayName: %v\n", displayName)
+
+		//Call helper func to calculate SHA-1 hash from file
+		sha1Checksum, err = getSha1(path)
+		if err != nil {
+			log.Printf("error calculating SHA-1 from file")
+		}
+		//TODO: Remove printf
+		fmt.Printf("SHA-1: %v\n", sha1Checksum)
+	}
+
 	return nil
 }
---
+++ crawler/helper.go
@@ -0,0 +1,96 @@
+package crawler
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"hash"
+	"internal-backend/utils"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// getCategory take a filepath and extract category based on path
+func getCategory(p string) (string, error) {
+	var (
+		err      error
+		docRoot  string //Documents disk path
+		relPath  string //Relative path from doc root
+		category string //Calculated category
+	)
+
+	//Read doc root from env
+	docRoot, err = utils.ReadEnv("DOC_ROOT")
+	if err != nil {
+		log.Fatalf("Error retrieving documents root from env")
+	}
+
+	// extract relative path from doc root
+	relPath, err = filepath.Rel(docRoot, p)
+	if err != nil {
+		return "", errors.New(fmt.Sprintf("crawler/helper/getCategory returned error while processing relative filepath: %v\n", err))
+	}
+
+	// extract category from relative path, stripping filename
+	category = filepath.Dir(relPath)
+
+	return category, nil
+}
+
+// getDisplayName take a path and calculate human readable file name, stripping useless char
+func getDisplayNameFromPath(p string) string {
+	var (
+		fileName                 string //Filename from path
+		extension                string //Filename extension
+		fileNameWithoutExtension string //Filename without ext
+		displayName              string //Calculated display name
+	)
+
+	// extract filename from path, trimming lead and trail spaced
+	fileName = strings.TrimSpace(filepath.Base(p))
+
+	// extract extension from filename
+	extension = filepath.Ext(p)
+
+	// trim extension from filename
+	fileNameWithoutExtension = strings.TrimSuffix(fileName, extension)
+
+	displayName = strings.ReplaceAll(fileNameWithoutExtension, "_", " ")
+	return displayName
+}
+
+// getSha1 take a valid file path and calculate SHA-1 checksum
+func getSha1(p string) (string, error) {
+	var (
+		err           error
+		file          *os.File  //In memory file
+		hashInterface hash.Hash //Hash interface to write to
+		hashInByte    []byte    //Calculated hash as []byte
+		checksum      string    //SHA-1 checksum
+	)
+
+	file, err = os.Open(p)
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	hashInterface = sha1.New()
+
+	if _, err = io.Copy(hashInterface, file); err != nil {
+		return "", err
+	}
+
+	// Calculate SAH-1
+	hashInByte = hashInterface.Sum(nil)
+
+	// Convert [] byte in string
+	checksum = hex.EncodeToString(hashInByte)
+
+	return checksum, nil
+}
